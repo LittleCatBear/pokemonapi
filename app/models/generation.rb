05b5cce6fbe6f305895a2d3647df1016c0b4163b
LittleCatBear <j.huguet9@gmail.com>
2023-06-08 20:27:52 -0400

    add Generation model validations and tests

--- app/models/generation.rb
+++ app/models/generation.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
+# Pokemon generation
 class Generation < ApplicationRecord
+  has_many :pokemons
+
+  validates :number,
+            presence: true,
+            numericality: { only_integer: true, greater_than: 0 },
+            uniqueness: true
 end
--- test/models/generation_test.rb
+++ test/models/generation_test.rb
@@ -1,7 +1,34 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class GenerationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @generation = Generation.new(number: 1)
+  end
+
+  test "should be valid" do
+    assert @generation.valid?
+  end
+
+  test "valid? returns false if number is not present" do
+    @generation.number = nil
+    assert_not @generation.valid?
+  end
+
+  test "valid? returns false if number is not an integer" do
+    @generation.number = 1.5
+    assert_not @generation.valid?
+  end
+
+  test "valid? returns false if number is not positive" do
+    @generation.number = -1
+    assert_not @generation.valid?
+  end
+
+  test "valid? returns false if number is not unique" do
+    duplicate_generation = @generation.dup
+    @generation.save
+    assert_not duplicate_generation.valid?
+  end
 end
