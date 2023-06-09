230ea4728fc4d4034c09060fde7d7c61173f39a4
LittleCatBear <j.huguet9@gmail.com>
2023-06-08 20:44:10 -0400

    Add type validations and tests

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
+# Pokemon type
 class Type < ApplicationRecord
+  has_many :pokemons, foreign_key: 'type1_id'
+  has_many :pokemons, foreign_key: 'type2_id'
+
+  validates :name, presence: true,
+                   length: { maximum: 100 },
+                   uniqueness: true
 end
--- test/models/type_test.rb
+++ test/models/type_test.rb
@@ -1,7 +1,29 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class TypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @type = Type.new(name: "Fire")
+  end
+
+  test "should be valid" do
+    assert @type.valid?
+  end
+
+  test "valid? returns false if name is not present" do
+    @type.name = ""
+    assert_not @type.valid?
+  end
+
+  test "valid? returns false if name is too long" do
+    @type.name = "a" * 101
+    assert_not @type.valid?
+  end
+
+  test "valid? returns false if name is not unique" do
+    duplicate_type = @type.dup
+    @type.save
+    assert_not duplicate_type.valid?
+  end
 end
