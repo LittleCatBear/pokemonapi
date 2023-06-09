c7ff9546e6312e32e561361aa80aac0d1a07be55
LittleCatBear <j.huguet9@gmail.com>
2023-06-08 20:49:17 -0400

    add pokemon validation and tests

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,2 +1,28 @@
+# frozen_string_literal: true
+
+# Pokemon
 class Pokemon < ApplicationRecord
+  belongs_to :type1, class_name: 'Type'
+  belongs_to :type2, class_name: 'Type', optional: true
+  belongs_to :generation
+
+  before_save :calculate_total
+
+  validates :number,
+            presence: true,
+            numericality: { only_integer: true, greater_than: 0 }
+  validates :name, presence: true, uniqueness: true
+  validates :hp, :attack, :defense, :sp_atk, :sp_def, :speed,
+            presence: true,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :legendary, inclusion: { in: [true, false] }
+
+  validates :type1, presence: true
+  validates :generation, presence: true
+
+  private
+
+  def calculate_total
+    self.total = hp + attack + defense + sp_atk + sp_def + speed
+  end
 end
--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -1,7 +1,60 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class PokemonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @type1 = Type.create(name: 'Fire')
+    @type2 = Type.create(name: 'Water')
+    @generation = Generation.create(number: 1)
+    @pokemon = Pokemon.new(
+      number: 1,
+      name: 'Bulbasaur',
+      hp: 45,
+      attack: 49,
+      defense: 49,
+      sp_atk: 65,
+      sp_def: 65,
+      speed: 45,
+      type1: @type1,
+      type2: @type2,
+      generation: @generation,
+      legendary: false
+    )
+  end
+
+  test "should be valid" do
+    assert @pokemon.valid?
+  end
+
+  test "should be valid without a type2" do
+    @pokemon.type2 = nil
+    assert @pokemon.valid?
+  end
+
+  test "valid? returns false if name is not present" do
+    @pokemon.name = nil
+    assert_not @pokemon.valid?
+  end
+
+  test "valid? returns false if name is not unique" do
+    duplicate_pokemon = @pokemon.dup
+    @pokemon.save
+    assert_not duplicate_pokemon.valid?
+  end
+
+  test "valid? returns false if total is not correctly calculated" do
+    @pokemon.save!
+    assert_equal @pokemon.total, @pokemon.hp + @pokemon.attack + @pokemon.defense + @pokemon.sp_atk + @pokemon.sp_def + @pokemon.speed
+  end
+
+  test "valid? returns false if type1 is missing" do
+    @pokemon.type1 = nil
+    assert_not @pokemon.valid?
+  end
+
+  test "valid? returns false if generation is missing" do
+    @pokemon.generation = nil
+    assert_not @pokemon.valid?
+  end
 end
