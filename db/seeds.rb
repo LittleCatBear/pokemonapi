7bd208bd4ded6f6969a9cea8e9419938ff7da346
LittleCatBear <j.huguet9@gmail.com>
2023-06-10 14:24:59 -0400

    add script to ingest pokemon csv file as seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,37 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 
 require 'csv'
 
 text = File.read(Rails.root.join('db', 'pokemon.csv'))
-pokemons = CSV.parse(text, headers: true, encoding: 'ISO-8859-1')
-pokemons.each do |pokemon|
-  puts pokemon.to_hash
+csv = CSV.parse(text, headers: true)
+csv.each_with_index do |row, index|
+  pokemon = Pokemon.find_or_initialize_by(name: row['Name'])
+
+  pokemon.number = row['#']
+  pokemon.hp = row['HP']
+  pokemon.attack = row['Attack']
+  pokemon.defense = row['Defense']
+  pokemon.sp_atk = row['Sp. Atk']
+  pokemon.sp_def = row['Sp. Def']
+  pokemon.speed = row['Speed']
+  pokemon.legendary = row['Legendary']
+
+  type1 = Type.find_or_create_by(name: row['Type 1'])
+  pokemon.type1 = type1
+
+  if row['Type 2']
+    type2 = Type.find_or_create_by(name: row['Type 2'])
+    pokemon.type2 = type2
+  end
+
+  generation = Generation.find_or_create_by(number: row['Generation'])
+  pokemon.generation = generation
+
+  if pokemon.save
+    puts "Successfully saved pokemon #{pokemon.name}, type1: #{pokemon.type1.name}, type2: #{pokemon.type2&.name}, generation: #{pokemon.generation.number}"
+  else
+    puts "Error: pokemon #{pokemon.name} at line #{index + 1} has errors #{pokemon.errors.full_messages}, skipping..."
+    next
+  end
 end
